0da6a5aa6903eca87470abafd72b7e0747517fae
Enno Rehling <enno.rehling@gmail.com>
2012-02-05 14:35:17 -0800

    Moving files around
    The old LuaSDL becomes the new LuaSDL
    Next, SDLX files should go into the daisy library. Two DLLs, oh my!

--- daisy/LuaSDL/dllmain.cpp
+++ daisy/LuaSDL/dllmain.cpp
@@ -3,9 +3,18 @@
 #include <lua.h>
 
 extern "C" {
-    int tolua_sdl_open(lua_State*);
+    int tolua_SDL_wr_open(lua_State*);
+    int tolua_SDL_video_wr_open(lua_State*);
+    int tolua_SDL_timer_wr_open(lua_State*);
+    int tolua_SDL_keyboard_wr_open(lua_State*);
+    int tolua_SDL_event_wr_open(lua_State*);
     int __declspec(dllexport) luaopen_LuaSDL (lua_State* L) {
-        return tolua_sdl_open(L);
+        tolua_SDL_wr_open(L);
+        tolua_SDL_video_wr_open(L);
+        tolua_SDL_timer_wr_open(L);
+        tolua_SDL_keyboard_wr_open(L);
+        tolua_SDL_event_wr_open(L);
+        return 0;
     };
 
 }
