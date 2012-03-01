4d25a92e242073884ed70cedf964da41dce7ea9b
Enno Rehling <enno.rehling@gmail.com>
2012-02-29 22:33:52 -0800

    include bindings in the android build
    main.lua does not currently do anything. why?

--- android-project/assets/main.lua
+++ android-project/assets/main.lua
@@ -1,1 +1,17 @@
-a=47
+SDL_Init(SDL_INIT_EVERYTHING)
+local w = 640
+local h = 480
+local rect = SDL_CreateRect()
+rect.x = 0
+rect.y = 0
+rect.w = w
+rect.h = h
+local fullscreen = 0x80000000
+local window = SDL_CreateWindow("SDL 2.0 Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_SHOWN)
+local screen = SDL_GetWindowSurface(window)
+local surface = SDL_ConvertSurface(screen, screen.format, SDL_RLEACCEL)
+local renderer = SDL_CreateRenderer(window, -1, 0)
+SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255)
+SDL_RenderClear(renderer)
+SDL_Delay(1000)
+SDL_Quit()
--- android-project/jni/src/Android.mk
+++ android-project/jni/src/Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 LUA_PATH := ../Lua
+MARIGOLD_PATH := ../../..
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/test \
 	$(LOCAL_PATH)/$(SDL_PATH)/include \
@@ -13,7 +14,9 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/test \
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
-	engine/main.c
+	$(subst $(LOCAL_PATH)/,, $(wildcard $(LOCAL_PATH)/../../../LuaSDL2/SDL_*_wr.c)) \
+	$(subst $(LOCAL_PATH)/,, $(wildcard $(LOCAL_PATH)/../../../LuaSDL2/*_helper.c)) \
+	engine/main.c 
 
 LOCAL_SHARED_LIBRARIES := SDL2 Lua
 
